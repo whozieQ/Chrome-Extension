290f4a2ef08e7a81c638837d131611b400e9f148
Susan Briggs <49916215+whozieQ@users.noreply.github.com>
2022-08-08 23:24:24 -0400

    Add final Save Tab button

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-})
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        saveLead(tabs[0].url)    
+    })
 
-function renderLeads() {
+})
+function saveLead(lead){
+    myLeads.push(lead)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render()
+}
+
+function render(arrayList) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < arrayList.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${arrayList[i]}'>
+                    ${arrayList[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render()
+})
+
+inputBtn.addEventListener("click", function() {
+    saveLead(inputEl.value)
+    inputEl.value = ""
+
+})
